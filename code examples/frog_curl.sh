f5347cb59018431a77fa79e4bf874434e0506b21
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2024-02-20 14:41:49 +0200

    Adding code examples/frog_curl.sh

---
+++ code examples/frog_curl.sh
@@ -0,0 +1,4 @@
+curl --user user:pass \
+'https://realtime.oxylabs.io/v1/queries' \
+-H "Content-Type: application/json" \
+-d '{"source": "universal_ecommerce", "url": "https://www.frog.ee/en/category/2359-top-50-best-sellers"}'
